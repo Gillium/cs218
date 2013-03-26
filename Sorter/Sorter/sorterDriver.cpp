7f516875473aa781c7b01762fdb63d82742a393c
Peter Piper <petermpiper@gmail.com>
2013-03-26 10:46:23 -0400

    Sorter driver bug-fixes

--- Sorter/Sorter/sorterDriver.cpp
+++ Sorter/Sorter/sorterDriver.cpp
@@ -16,7 +16,7 @@ double diffclock(clock_t clock1, clock_t clock2)
 	double diffticks = clock1 - clock2;
 	double diffms = (diffticks * 1000) / CLOCKS_PER_SEC;
 	return diffms;
-} 
+}
 
 int main()
 {
