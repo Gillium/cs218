dc04aaf01918a34425e3658bef2e50b160891736
Peter Piper <petermpiper@gmail.com>
2013-04-13 13:54:26 -0400

    Graph GraphType header

--- Graph/Graph/graphType.h
+++ Graph/Graph/graphType.h
@@ -1,6 +1,26 @@
-template <class ItemType>
-class GraphType
-{
+#include <cstdlib>
+#include <sys/stat.h>
+#ifndef MYGRAPHTYPE
+#define MYGRAPHTYPE
+
+using namespace std;
+
+static const MAX_VERTICES = 50;
+
+// Exception class used by Search and DeleteVertex when vertex is not found
+class NotFound{
+};
+
+// EdgeValueType must be a numeric type
+template <class VertexType, class EdgeValueType>
+struct EdgeType {
+	VertexType fromVertex;
+	VertexType toVertex;
+	EdgeValueType weight;
+};
+
+template <class VertexType, class EdgeValueType>
+class GraphType {
 public:
 	GraphType();
 	// Constructor
@@ -10,17 +30,17 @@ public:
 	~GraphType();
 	// Deconstructor
 	// Pre:	 GraphType is initialized
-	// Post: 
+	// Post: deletes vertices, edges
 
-	GraphType();
+	GraphType(const GraphType<VertexType>& acopy);
 	// Copy constructor
 	// Pre:	 GraphType is initialized
-	// Post:
+	// Post: Graph is initialized with values from acopy
 
 	void MakeEmpty();
 	// Function: Initializes the graph to an empty state
 	// Pre:		 GraphType is initialized
-	// Post:	 Graph is empty
+	// Post:	 marks, vertices, edges are empty, numVertices is zero
 
 	bool IsEmpty();
 	// Function: Tests whether the graph is empty
@@ -34,78 +54,80 @@ public:
 
 	void AddVertex(VertexType vertex);
 	// Function: Adds vertex to the graph
-	// Pre:		 Graph is not full
-	// Post:	 vertex is in V(graph)
+	// Pre:		 GraphType is initialized, Graph is not full
+	// Post:	 vertex is in vertices, numVertices is incremented
 
-	void AddEdge(VertexType fromVertex, VertexType toVertex);
+	void AddEdge(VertexType fromVertex, VertexType toVertex, EdgeValueType weight);
 	// Function: Adds an edge with the specified weight from fromVertex to toVertex
-	// Pre:		 fromVertex and toVertex are in V(graph)
-	// Post:	 (fromVertex, toVertex) is in E(graph) with the specified weight
+	// Pre:		 GraphType is initialized, fromVertex and toVertex are in vertices
+	// Post:	 (fromVertex, toVertex) is in edges with the specified weight
 
 	EdgeValueType GetWeight(VertexType fromVertex, VertexType toVertex);
-	// Function: Determines the weight of the edge from fromVertex to toVertex
-	// Pre:		 fromVertex to toVertex
+	// Function: Returns the weight of the edge from fromVertex to toVertex
+	// Pre:		 GraphType is initialized, fromVertex and toVertex are vertices
 	// Post:	 Function value = weight of edge from fromVertex to toVertex if edge exists, if edge does not exist function value = special "null-edge" value
 
 	void GetToVertices(VertexType vertex, QueType& vertexQ);
 	// Function: Returns a queue of the vertices that are adjacent from vertex
-	// Pre:		 vertex is in V(graph)
+	// Pre:		 GraphType is initialized, vertex is in vertices
 	// Post:	 vertexQ contains the names of all vertices that are adjacent from vertex
 
 	int Search(VertexType vertex);
 	// Function: Returns the index for a vertex in the graph
 	// Pre:		 GraphType is initialized
-	// Post:	 
+	// Post:	 if found Function value = (index of vertex), otherwise throw NotFound exception
 
-	DeleteVertex();
-	// Function:
-	// Pre:
-	// Post:
+	void DeleteVertex(VertexType vertex);
+	// Function: Deletes vertex from graph and associated edges
+	// Pre:		 GraphType is initialized
+	// Post:	 vertices does not contain vertex and numVertices is decremented, NotFound exception thrown if vertex is not in vertices before function call
 
-	DeleteEdge();
-	// Function:
-	// Pre:
-	// Post:
+	void DeleteEdge(VertexType fromVertex, VertexType toVertex);
+	// Function: Deletes edge between fromVertex and toVertex
+	// Pre:		 GraphType is initialized
+	// Post:	 edges does not contain edge, NotFound exception thrown if edge is not in edges before function call
 
-	GetVerticesCount();
-	// Function:
-	// Pre:
-	// Post:
+	int GetVerticesCount();
+	// Function: Gets the number of vertices in the graph
+	// Pre:		 GraphType is initialized
+	// Post:	 Function return = (numVertices)
 
-	Display();
-	// Function:
-	// Pre:
-	// Post:
+	bool IsConnected();
+	// Function: Determines if the graph is connected
+	// Pre:		 GraphType is initialized
+	// Post:	 Function value = (is graph connected)
 
-	IsConnected();
-	// Function:
-	// Pre:
-	// Post:
+	QueueType<EdgeType> ShortestPath(VertexType fromVertex, VertexType toVertex);
+	// Function: Finds the shortest path between two vertices
+	// Pre:		 GraphType is intialized
+	// Post:	 Function value = (queue containing edges of shortest path)
 
-	FindShortestPath();
-	// Function:
-	// Pre:
-	// Post:
-
-	FindMinimalSpanningTree();
-	// Function:
-	// Pre:
-	// Post:
+	GraphType<VertexType, EdgeValueType> FindMinimalSpanningTree();
+	// Function: Finds the subgraph of the graph containing the shortest paths between the vertices
+	// Pre:		 GraphType is intialized
+	// Post:	 Function value = (graph describing minimal spanning tree)
 
 	void ClearMarks();
 	// Function: Sets marks for all vertices to false
-	// Pre:
+	// Pre:		 GraphType is initialized
 	// Post:	 All marks have been set to false
 
 	void MarkVertex(VertexType vertex);
 	// Function: Sets mark for vertex to true
-	// Pre:		 vertex is in V(graph)
+	// Pre:		 vertex is in vertices
 	// Post:	 IsMarked(vertex) is true
 
 	bool IsMarked(VertexType vertex);
 	// Function: Determines if vertex has been marked
-	// Pre:		 vertex is in V(graph)
+	// Pre:		 vertex is in vertices
 	// Post:	 Function value = (vertex is marked true)
 
 private:
+	int numVertices;
+	VertexType vertices[MAX_VERTICES];
+	int edges[MAX_VERTICES][MAX_VERTICES];
+	bool* marks;
 };
+
+#include "graphType.template"
+#endif
